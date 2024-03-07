f4875bc87c89705a06a074405a142d408fcadb80
Leidy Castaño <leidy.castanoc@udea.edu.co>
2024-03-07 18:08:37 -0500

    service updated

--- services/index.ts
+++
@@ -1,9 +0,0 @@
-const API_URL = 'http://demo7771177.mockable.io/destination';
-
-
-const API_SERVICES = {
-    users: `${API_URL}`,
-};
-
-const fetcher = (url:string) => fetch(url).then((res)=>res.json());
-export {API_SERVICES,  fetcher};
