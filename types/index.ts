563764b8b834b3554d8d7052c593e97cc4faa3c1
soyudea <omara.torres@udea.edu.co>
2024-03-20 10:00:32 -0500

    Actualizando usuario y roles

--- types/index.ts
+++ types/index.ts
@@ -10,3 +10,19 @@ export interface User{
 export interface UserQuery {
     users: User[];
 }
+import { RolesQuery } from '@/types';
+import useSWR from 'swr';
+
+const useGetRoles=()=> {
+    const {data, isLoading, error } = useSWR<RolesQuery>(
+    API_SERVICES.roles,
+    fetcher
+    );
+    return{
+        roles: data?.roles,
+        isLoading,
+        error,
+    };
+};
+
+export{useGetRoles};
